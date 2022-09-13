bf2ce5ed54a451ddc6f5527a1ab8bf7e06fdb9d7
nguyenthuyit <nguyenthithuy.int@gmail.com>
2022-09-13 12:12:34 +0700

    upload

--- main.js
+++ main.js
@@ -17,3 +17,19 @@ function openMenu(evt,nameMenu){
 const now = new Date()
 now.setMinutes(now .getMinutes()- now.getTimezoneOffset())
 document.getElementById('dt').value = now.toISOString().slice(0,16)
+// openMenu-mobile
+
+    var nav = document.getElementById('nav')
+    var iconMobile = document.getElementById('icon-mobile')
+    function openNav(){
+          nav.style.display ='block'
+          iconMobile.style.display = 'none' 
+    }
+    var navItems = document.querySelectorAll('#nav li a[href *="#"]')
+    for (var i = 0; i < navItems.length; i++){
+        var navItem = navItems[i]
+        navItem.onclick = function(){
+            nav.style.display ='none'
+          iconMobile.style.display = 'block' 
+        }
+    }
